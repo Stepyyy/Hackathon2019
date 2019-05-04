2018d0e045ed682d8eff568a7ef49a20a5dc2cf2
Unknown <ryan.martin.gawn@outlook.com>
2019-05-04 15:52:56 +1200

    Added db

--- watchful/server.js
+++ watchful/server.js
@@ -1,7 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/watchfulstore.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the watchful database.');
+  });
 
-app.get('/', (req, res) => res.send('Welcome to Watchful. Enjoy the Watch Parties.'))
+let partysql = 'SELECT title, date, hostName FROM Party';
+
+db.all(partysql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row.title, row.date, row.hostName);
+    });
+  });
+
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
+app.get('/', (req, res) => res.send('Life'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
